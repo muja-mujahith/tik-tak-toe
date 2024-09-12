ba8fef4719e5f3909393893de35426017b90e9b6
muja-mujahith <mujahith18862.new@gmail.com>
2024-09-12 18:29:52 +0530

    TikTacToe

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import TicTakToe from './components/TicTakToe/TikTakToe';
+
+function App() {
+  return (
+    <div>
+      <TicTakToe />
+    </div>
+  );
+}
+
+export default App;
